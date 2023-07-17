190f16209af75e026a1d144d7be2802eabdbb528
the bob <ibrahim.mneimneh3@gmail.com>
2023-07-18 00:09:15 +0300

    homepage-phase2

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,11 +3,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
 
 const HomePage = () => (
     <>
     <Container fluid >
       <Row >
+
         <Col md={4} className='padding-0'>
            <div class="hicontainer">
             <div class="hi">
@@ -20,11 +23,22 @@ const HomePage = () => (
         <Image src="img/ibrahim.jpg"  fluid />
         
         </Col>
+
       </Row>
       <Row>
-        <Col md={12} className='cv'>
-            <div>
+        <Col md={12} className='cv d-flex flex-column justify-content-center align-items-center '>
+            <div className='cv-title'>
                 <h1>My CV:</h1>
+            </div>
+            <div>
+                <button className="button-cv-pushable">
+                <span className="button-cv-shadow"></span>
+                <span className="button-cv-edge"></span>
+                <span className="button-cv-front text">
+                Download Ibrahim's CV
+                <FontAwesomeIcon icon={faFile} style={{color: "#ffffff",paddingLeft:"15px"}} />
+                </span>
+                </button>
             </div>
         </Col>
       </Row>
